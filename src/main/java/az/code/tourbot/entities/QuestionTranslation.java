d4fecd4667e5d790fbc4d97f25a118f9db5417bc
fuadhajiyev <hajiyev1996@gmail.com>
2023-02-16 23:13:05 +0400

    added repository

--- src/main/java/az/code/tourbot/entities/QuestionTranslation.java
+++ src/main/java/az/code/tourbot/entities/QuestionTranslation.java
@@ -1,15 +1,11 @@
 package az.code.tourbot.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.List;
+import lombok.*;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
---
+++ src/main/java/az/code/tourbot/repositories/LanguageRepository.java
@@ -0,0 +1,9 @@
+package az.code.tourbot.repositories;
+
+import az.code.tourbot.entities.Language;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LanguageRepository extends JpaRepository<Language, Integer> {
+}
